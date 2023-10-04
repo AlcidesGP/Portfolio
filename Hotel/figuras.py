5c6e40ea1e93ac91859caebcfa26c16ec4dc27f1
AlcidesGP <146877995+AlcidesGP@users.noreply.github.com>
2023-10-03 21:20:24 -0300

    Atualização

--- Hotel/figuras.py
+++ Hotel/figuras.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
-
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,40 +9,9 @@ from mycolorpy import colorlist as mcp
 import matplotlib.gridspec as gridspec
 
 
-# In[6]:
 
 
-def pie_0(dados: list,titulo: str, target: str) -> None:
-    """
-    Função irá retornar o gráfico de pizza do alvo categórico, separando-o por cor e distância, além de adicionar
-    as proporções visualmente no gráfico.
-    
-    dados : Dataframe que será trabalhodo
-    Titulo : Título no gráfico.
-    Target : O alvo de classificação.
-    
-    """
-    dt = dados.copy()
-    fig = plt.figure(figsize=(10, 5)) # Criando Imagem
-    colors=mcp.gen_color(cmap="Blues_r",n=5)
-    outer = gridspec.GridSpec(1, # Rows
-                              2, # Cols
-                              wspace=0.3, hspace=0.2) # Espaço entre os quadrantes da imagem
-    fig.suptitle(titulo, color='grey', size=20)
-    # PLOT 
-    
-    fig.add_subplot(outer[0, :])
-    dt = dt[target].value_counts()
-    x = dt.values
-    y = dt.index
-
-    pie = plt.pie(x = x, labels=y, autopct="%1.f%%", shadow=True, explode=(0,0.1), startangle=90, colors=colors[0:])
-
-
-# In[1]:
-
-
-def barras_(dados: list, titulo: str, tamanho: tuple, t_imagem: tuple) -> None:
+def barras_(dados: list, titulo: str, tamanho: tuple, t_imagem: tuple, target: str = None) -> None:
     """
     ---- Propósito da função -----
     Retornar uma imagem contendo a frequência de cada categoria dos dados qualitativos.
@@ -54,6 +21,7 @@ def barras_(dados: list, titulo: str, tamanho: tuple, t_imagem: tuple) -> None:
     titulo => string; Nome do título do gráfico.
     tamanho => tuple; Quantidade de linhas e colunas que a imagem vai ter.
     t_imagem => tuple; contendo a quantidade de linhas e colunas que a imagem deve conter.
+    target => string; Nome da coluna alvo, se categórico
     
     """
     
@@ -63,8 +31,7 @@ def barras_(dados: list, titulo: str, tamanho: tuple, t_imagem: tuple) -> None:
     # Configurações Iniciais
     grafico = plt.figure(figsize=t_imagem) # Definir tamanho da figura
     plt.suptitle(titulo, fontsize=30, horizontalalignment='center' ,color='grey') # Título
-    plt.subplots_adjust(top=0.8, # Espaço entre o título e os gráficos
-                        wspace = 0.5, # Espaço vertical entre os gráficos
+    plt.subplots_adjust(wspace = 0.5, # Espaço vertical entre os gráficos
                         hspace = 0.5) # Espaço horizontal entre os gráficos
 
     column = 0 # Para contar qual coluna está sendo usada.
@@ -77,14 +44,20 @@ def barras_(dados: list, titulo: str, tamanho: tuple, t_imagem: tuple) -> None:
                 g = (tamanho[1]) - pos_y # Quantas colunas será o tamanho do gráfico na imagem.
                 ax = plt.subplot2grid(tamanho, pos, colspan=g) # Definir a AX de cada gráfico.
                 last = True # Contador para quebrar a função em caso de ser a última coluna
-                color = 'red' # Cor do palette
+
             else:
                 pos = (pos_x, pos_y) # Definir o tamanho da imagem (linhas e colunas)
                 ax = plt.subplot2grid(tamanho, pos) # Definir a AX de cada gráfico.
                 last = False
+            
+            # Essa função irá alterar a cor do gráfico que for o target.
+            if target == dt.columns[column]:
+                color = 'red' # Cor do palette
+            else:
                 color = '#78C' # Cor do palette
-            
-            
+
+
+                
             # Plot das barras
             valores = dt[dt.columns[column]].value_counts().values # O valor da frequência.
             categorias = dt[dt.columns[column]].value_counts().index # A categoria da frequência.
@@ -133,7 +106,6 @@ def barras_(dados: list, titulo: str, tamanho: tuple, t_imagem: tuple) -> None:
     return plt.show()
 
 
-# In[4]:
 
 
 print('Importação realizada com sucesso.')
