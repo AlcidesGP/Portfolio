be1479638c391cfb4d086923296165db349402e0
Alcides Gabriel <alcidespcoutinho@gmail.com>
2023-11-01 23:20:46 -0300

    Update app.py

--- EBAC/Streamlit/app.py
+++ EBAC/Streamlit/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import plotly.express as px
 import plotly.subplots as sp
 import plotly.graph_objects as go
