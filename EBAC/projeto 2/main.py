4101da56d341104bad365e1411052633219c85a4
Alcides Gabriel <alcidespcoutinho@gmail.com>
2023-11-13 09:38:31 -0300

    att

--- EBAC/projeto 2/main.py
+++ EBAC/projeto 2/main.py
@@ -1,8 +1,27 @@
 import streamlit as st
-
+from streamlit_pandas_profiling import st_profile_report
+from ydata_profiling import ProfileReport
 import pandas as pd
 import numpy as np
 
-st.set_page_config(layout="wide")
-st.title('Projeto 2')
-st.button('Hit me')
+st.set_page_config(page_title='Análise de renda', page_icon=':bar_chart:', layout='wide')
+st.title('Projeto 2 - Análise estatística dos dados')
+st.subheader("Abaixo estão as informações estatística do comportamento de cada atributo dos dados renda.")
+st.write("""O botão abaixo irá redirecionar-te para a página do notebook de todo o projeto de previsão, aonde se encontra 
+         os trabalho realizado com base no método Crisp-DM.""")
+st.link_button("Notebook do modelo", "https://nbviewer.org/github/AlcidesGP/Portfolio/blob/main/EBAC/projeto%202/projeto-2.ipynb")
+
+@st.cache_data
+def carregar_dados():
+    data = pd.read_csv("C:\\Users\\alcid\\GitHub\\Portfolio\\EBAC\\projeto 2\\input\\previsao_de_renda.csv")
+    return data
+
+renda = carregar_dados()
+
+renda = renda.drop("Unnamed: 0", axis=1)
+ID = renda.pop('id_cliente')
+
+#st.sidebar.success('Select a page above.')
+profile = ProfileReport(renda, title='Sumário da renda')
+
+st_profile_report(profile)
---
+++ EBAC/projeto 2/pages/analise_binaria.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+import plotly.express as px
+import plotly.subplots as sp
+import plotly.graph_objects as go
+
+st.set_page_config(page_title='Análise de renda', page_icon=':bar_chart:', layout='wide')
+st.title('Projeto 2 - Análise binária')
+st.write("Abaixo estão as informações estatística do comportamento de cada atributo dos dados renda.")
+
+@st.cache_data
+def carregar_dados():
+    data = pd.read_csv("C:\\Users\\alcid\\GitHub\\Portfolio\\EBAC\\projeto 2\\input\\previsao_de_renda.csv")
+    return data
+
+
+renda = carregar_dados()
+renda = renda.drop("Unnamed: 0", axis=1)
+ID = renda.pop('id_cliente')
+
+
+# ----- Categórico
+st.subheader('Atributos categóricos')
+
+locais = pd.concat([renda.loc[:,['qt_pessoas_residencia','qtd_filhos']].astype(str), renda.select_dtypes(exclude=np.number)]).columns
+local_ = st.selectbox(
+    'Qual é o atributo categórico a ser estudado?',
+    (locais)
+)
+
+
+def imagem_cat() -> None:
+    dt = renda.copy()
+    dt['Faixa_renda'] = pd.qcut(renda['renda'], q=4).astype(str)
+    fig = px.box(data_frame=dt, orientation='h', x='renda',color=local_)
+    st.plotly_chart(fig)
+    dt =  dt.groupby([local_,'Faixa_renda'], as_index=True)[local_].count()
+    dt.name = 'Frequencia'
+    dt = dt.reset_index()
+    fig = px.bar(data_frame=dt, x=local_,y='Frequencia',color='Faixa_renda')
+    st.plotly_chart(fig)
+
+imagem_cat()
+
+
+# ---- Numérico
+
+st.subheader('Atributos numéricos')
+locais = renda.select_dtypes(include=np.number).drop(['qtd_filhos','qt_pessoas_residencia','renda'], axis=1).columns
+local_ = st.selectbox(
+    'Qual é o atributo numérico a ser estudado?',
+    (locais)
+)
+
+
+def imagem_num() -> None:
+    dt = renda.copy()
+    dt = dt.fillna(0)
+    dt[f'Faixa_{local_}'] = pd.qcut(dt[local_], q=4).astype(str)
+    fig = px.box(data_frame=dt, orientation='h', x='renda',color=f'Faixa_{local_}')
+    st.plotly_chart(fig)
+    fig = px.scatter(data_frame=dt, x=local_, y='renda')
+    st.plotly_chart(fig)
+
+
+imagem_num()
