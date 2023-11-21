7d58c21d728d8f69c8edcda07056b5c38b867d74
Alcides Gabriel <alcidespcoutinho@gmail.com>
2023-11-21 16:47:09 -0300

    a

--- EBAC/Streamlit_Modulo_19_ Bank/app_m_19_01.py
+++ EBAC/Streamlit_Modulo_19_ Bank/app_m_19_01.py
@@ -1,6 +1,7 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
+
 
 import matplotlib.pyplot as plt
 import seaborn as sns
--- EBAC/m19-streamlit/main.py
+++ EBAC/m19-streamlit/main.py
@@ -4,3 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+### Configuração da página
+st.set_page_config(
+    page_title="Marketing Analyses",
+    layout='wide',
+    initial_sidebar_state='expanded',
+)
+
+### ------------------------------------- Funções Base  ---------------------------------------------------------
+@st.cache_data()
+def loader():
+    return pd.read_csv("./bank-additional-full.csv", sep=';', header=0)
+    # return  pd.read_csv("C:\\Users\\alcid\\GitHub\\Portfolio\\EBAC\\Streamlit - Módulo 19 - Bank\\bank-additional-full.csv", sep=';')
+
+@st.cache_data()
+def selecao_atributos(relatorio, col, selecionados):
+    if 'all' in selecionados:
+        return relatorio
+    else:
+        return relatorio.loc[relatorio[col].isin(selecionados)].reset_index(drop=True)
