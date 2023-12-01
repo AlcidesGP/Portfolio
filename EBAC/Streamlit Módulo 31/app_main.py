5b4b6cad379ba3f927199d17fd0040ab65aa2588
Alcides Gabriel <alcidespcoutinho@gmail.com>
2023-12-01 16:59:04 -0300

    a

--- EBAC/Streamlit Módulo 31/app_main.py
+++ EBAC/Streamlit Módulo 31/app_main.py
@@ -1,7 +1,5 @@
-# Imports
+
 import streamlit         as st
 import pandas            as pd
-import seaborn           as sns
-import matplotlib.pyplot as plt
 
-df = st.sidebar.file_uploader('dados', type=['csv','xlsx'])
+
